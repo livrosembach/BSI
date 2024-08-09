188d994005283c6ac88712c3edfaab2ba67b70f2
Livia Rosembach Oliveira <rosembach.livia@gmail.com>
2024-08-09 15:01:34 -0300

    Exercicio de Java

--- 2°período/POO/pbl02/src/Main.java
+++ 2°período/POO/pbl02/src/Main.java
@@ -1,41 +1,44 @@
+import java.util.Locale;
 import java.util.Scanner;
 import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.US);
 
-                Scanner input = new Scanner(System.in);
-                System.out.println("Digite o valor de a:");
-                double a = input.nextDouble();
-                System.out.println("Digite o valor de b:");
-                double b = input.nextDouble();
+        System.out.println("Digite o valor de a:");
+        double a = input.nextDouble();
+        System.out.println("Digite o valor de b:");
+        double b = input.nextDouble();
 
-                if(a <= b){
-                    System.out.println("Digite o valor de n:");
-                    double n = input.nextDouble();
-                    if(n > 0){
-                        double totalArea = 0;
-                        double x = 0;
-                        double h = (b - a) / n;
-                        double y1 = f(x);
-                        int i = 0;
-                        while(i < n){
-                            x += h;
-                            double y2 = f(x);
-                            double trapezoidArea = ((y1 + y2) / 2) * h;
-                            y1 = y2;
-                            i++;
-                        }
-                        System.out.println(totalArea);
-                    }
-                    else
-                        System.out.println("O n tem que ser positivo");
+        if(a <= b){
+            System.out.println("Digite o valor de n:");
+            double n = input.nextDouble();
+            if(n > 0){
+                double totalArea = 0;
+                double x = a;
+                double h = (b - a) / n;
+                double y1 = f(x);
+                int i = 0;
+                while(i < n){
+                    x += h;
+                    double y2 = f(x);
+                    double trapezoidArea = ((y1 + y2) / 2) * h;
+                    totalArea += trapezoidArea;
+                    y1 = y2;
+                    i++;
                 }
-                else
-                    System.out.println("O valor de a deve ser menor ou igual ao b");
+                System.out.println(totalArea);
+            }
+            else
+                System.out.println("O n tem que ser positivo");
+        }
+        else
+            System.out.println("O valor de a deve ser menor ou igual ao b");
 
     }
     public static double f( double x){
-    return 2*Math.sin(x) + Math.cos(x) / 3;
+        return 2*Math.sin(x) + Math.cos(x) / 3;
     }
 }
