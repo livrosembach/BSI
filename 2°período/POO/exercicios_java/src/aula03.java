1000853b9253623195235eeb9ba0e365c4be4560
Livia Rosembach Oliveira <rosembach.livia@gmail.com>
2024-08-19 22:01:10 -0300

    aula 19/08

---
+++ 2°período/POO/exercicios_java/src/aula03.java
@@ -0,0 +1,52 @@
+class Album {
+    String genero;
+    int ano;
+    String  nome;
+    String  artista;
+    Musica[] musicas = new Musica[5];
+
+    public Album(String genero, int ano, String nome, String artista, Musica[] musicas){
+        this.genero = genero;
+        this.ano = ano;
+        this.nome = nome;
+        this.artista = artista;
+        this.musicas = musicas;
+    }
+
+    public void descricao(){
+        System.out.printf("Gênero: %s \n Ano: %s \n Nome: %s  \n Artista: %s \n", this.genero, this.ano, this.nome,this.artista);
+    }
+}
+class Musica {
+    String titulo;
+    double duracao;
+
+    public Musica(String titulo, double duracao){
+        this.titulo = titulo;
+        this.duracao = duracao;
+    }
+
+    public void tocarMusica(){
+        System.out.printf("Titulo: %s \n Duração %s \n", this.titulo, this.duracao);
+    }
+}
+
+class mainAula03 {
+    public static void main(String[] args) {
+        Musica[] musicas = new Musica[5];
+
+        musicas[0] = new Musica("Never mind",2.23);
+        musicas[1] = new Musica("Vibing", 3.32);
+        musicas[2] = new Musica("Mindfulness", 2.14);
+        musicas[3] = new Musica("Never Again", 3.24);
+        musicas[4] = new Musica("Mind Vibes", 2.56);
+        Album a1 = new Album("Jazz", 2021, "Vibe", "Josefina",musicas);
+
+        a1.descricao();
+
+        for(int i = 0; i < 5; i++){
+            Musica m = musicas[i];
+            m.tocarMusica();
+        }
+    }
+}
---
+++ 2°período/POO/exercicios_java/src/pbl02B.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class pbl02B {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.US);
+
+        System.out.println("Digite o número do primeiro termo da PA:");
+        double a_1 = input.nextDouble();
+        System.out.println("Digite a razão da PA:");
+        double r = input.nextDouble();
+        System.out.println("Digite o número de termos da PA:");
+        int n = input.nextInt();
+        while(n < 0){
+            System.out.println("O número de termos tem que ser positivo. Digite novamente:");
+            n = input.nextInt();
+        }
+        double a_n = a_1 + (n - 1) * r;
+        System.out.println("O último termo da PA é: " + a_n);
+        double s_n = (a_1 + a_n) * n / 2;
+        System.out.println("A soma dos termos da PA é: " + s_n);
+    }}
