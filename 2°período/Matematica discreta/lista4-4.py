b44f7004504f9b01a743a1eb1098f16ccab1c673
Livia Rosembach Oliveira <rosembach.livia@gmail.com>
2024-10-16 22:00:45 -0300

    Lista 4 - Matemática Discreta

---
+++ 2°período/Matematica discreta/lista4-4.py
@@ -0,0 +1,14 @@
+somatorio = 0
+for x in range(3):
+    for y in range(2):
+        somatorio += (x * y) - 5
+
+print(somatorio)
+
+somatorio = 0
+
+for x in range(2, 5):
+    for y in range(2,3):
+        somatorio += (x + y)**2
+
+print(somatorio)
