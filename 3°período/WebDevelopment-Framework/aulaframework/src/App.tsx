d3ebaeac494ae426f334555f9511a3ed83b9d8a8
Livia Rosembach Oliveira <rosembach.livia@gmail.com>
2025-02-26 22:35:48 -0300

    aulas 26/02 - react e cisco pakect tracer

--- 3°período/WebDevelopment-Framework/aulaframework/src/App.tsx
+++ 3°período/WebDevelopment-Framework/aulaframework/src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Menu from './layout/Menu'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Menu></Menu>
+      <div>
+      <form>
+  <div className="mb-3">
+    <label className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label  className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div>
+  <div className="mb-3 form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+    <label className="form-check-label" >Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+      </div>
     </div>
   );
 }
